aecd3b7f6e546099dabaf5ad0dddab28b8c22cf0
leoli7405@gmail.com <leoli7405@gmail.com>
2025-08-24 01:16:18 -0700

    updated try catch block in python

--- CloudflareBypasser.py
+++ CloudflareBypasser.py
@@ -110,52 +110,59 @@ class CloudflareBypasser:
         node_script = "/Users/leoli/WebstormProjects/job-hunt/services/google/getEmails.ts"
 
         if login: 
-                self.tryBypass()
-                print('Logging in.')
-                
-                emailInput = self.driver.ele('css:[id="ifl-InputFormField-:r0:"]')
-                emailInput.input("raveoli22@gmail.com")
-                
-                self.clickIfVisible('[data-tn-element="auth-page-email-submit-button"]', 'Continue')
-                self.tryBypass()
-                self.clickIfVisible('[data-tn-element="auth-page-email-submit-button"]', 'Continue')
-
-                signInWithCodeInstead = self.driver.ele('css:[data-tn-element="auth-page-google-password-fallback"]')
-                signInWithCodeInstead.click()
-
-                time.sleep(10)
-                print("Waiting 10s for new code...")
-
-                # Execute the Node.js script
                 try:
-                    cmd = ["tsx", node_script, "raveoli22"]
-                    res = subprocess.run(cmd, capture_output=True, text=True, check=True)
-                    out = res.stdout.strip()
-                    err = res.stderr.strip()
-                    codes = out.replace('OTP code: ','')
-                    codesJson = json.loads(codes)
-                except subprocess.CalledProcessError as e:
-                    print("Email Script STDOUT ->", e.stdout)
-                    print("Email Script STDERR ->", e.stderr)
-                    print("Email Script STDERR (original) ->", err)
-                    raise
-                
-                if codesJson:
-                    print("TOKEN", codesJson[0])
-                    tokenInput = self.driver.ele('css:[aria-describedby="label-passcode-input-error"]')
-                    tokenInput.input(codesJson[0])
-                    
-                    self.clickIfVisible('[data-tn-element="otp-verify-login-submit-button"]', 'Signin')
                     self.tryBypass()
-                    self.clickIfVisible('[data-tn-element="otp-verify-login-submit-button"]', 'Signin')
-                    self.clickIfVisible('[id="pass-WebAuthn-continue-button"]', 'Not Now')
+                    print('Logging in.')
+                    
+                    emailInput = self.driver.ele('css:[id="ifl-InputFormField-:r0:"]')
+                    emailInput.input("leoli7405@gmail.com")
+                    
+                    self.clickIfVisible('[data-tn-element="auth-page-email-submit-button"]', 'Continue')
+                    self.tryBypass()
+                    self.clickIfVisible('[data-tn-element="auth-page-email-submit-button"]', 'Continue')
 
-                    self.driver.get('https://www.indeed.com')
-                    self.driver.wait.ele_displayed('css:[id="AccountMenu"]', timeout=30)
-                    accountMenu = self.driver.ele('css:[id="AccountMenu"]')
-                    accountMenu.click()
-                else: 
-                    self.log_message("Email Script Failed.")
+                    self.driver.wait.ele_displayed("css:{}".format('[data-tn-element="auth-page-google-password-fallback"]'), timeout=2)
+                    signInWithCodeInstead = self.driver.ele('css:[data-tn-element="auth-page-google-password-fallback"]')
+                    signInWithCodeInstead.click()
+
+                    time.sleep(10)
+                    print("Waiting 10s for new code...")
+
+                    # Execute the Node.js script
+                    try:
+                        cmd = ["tsx", node_script]
+                        res = subprocess.run(cmd, capture_output=True, text=True, check=True)
+                        out = res.stdout.strip()
+                        err = res.stderr.strip()
+                        codes = out.replace('OTP code: ','')
+                        codesJson = json.loads(codes)
+                    except subprocess.CalledProcessError as e:
+                        print("Email Script STDOUT ->", e.stdout)
+                        print("Email Script STDERR ->", e.stderr)
+                        print("Email Script STDERR (original) ->", err)
+                        raise
+                    
+                    if codesJson:
+                        print("TOKEN", codesJson[0])
+                        tokenInput = self.driver.ele('css:[aria-describedby="label-passcode-input-error"]')
+                        time.sleep(10)
+                        tokenInput.input(codesJson[0])
+                        
+                        self.clickIfVisible('[data-tn-element="otp-verify-login-submit-button"]', 'Signin')
+                        self.tryBypass()
+                        self.clickIfVisible('[data-tn-element="otp-verify-login-submit-button"]', 'Signin')
+                        self.clickIfVisible('[id="pass-WebAuthn-continue-button"]', 'Not Now')
+
+                        self.driver.get('https://www.indeed.com')
+                        self.driver.wait.ele_displayed('css:[id="AccountMenu"]', timeout=30)
+                        accountMenu = self.driver.ele('css:[id="AccountMenu"]')
+                        accountMenu.click()
+                    else: 
+                        self.log_message("Email Script Failed.")
+                
+                except Exception as e:
+                    print(e)
+                    raise e
         else:
             self.tryBypass()
             if self.is_bypassed():
